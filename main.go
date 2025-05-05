068f1ff1f002ad8119f4cde4f027b874d4c94de1
Ronald Ngounou <74538524+ronaldngounou@users.noreply.github.com>
2025-05-04 21:46:29 -0700

    Complete part2 quiz1
    Signed-off-by: Ronald Ngounou <74538524+ronaldngounou@users.noreply.github.com>

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 )
 
 type Problem struct {
@@ -16,6 +17,7 @@ type Problem struct {
 
 func main() {
 	csvFileName := flag.String("csv", "problems.csv", "a csv file in the format question, answer")
+	timeLimit := flag.Int("time", 30, "time to answer each question in seconds")
 	flag.Parse()
 	file, err := os.Open(*csvFileName)
 	if err != nil {
@@ -33,13 +35,27 @@ func main() {
 	problems := parseLines(records)
 	correct := 0
 
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+
 	for i, problem := range problems {
 		fmt.Printf("Problem #%d = %s \n", i+1, problem.question)
-		var answer string
-		fmt.Scanf("%s \n", &answer)
-		if answer == problem.answer {
-			correct += 1
+		answerCh := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanf("%s \n", &answer)
+			answerCh <- answer // arrow is always pointing towards the way data is moving
+		}()
+		select {
+		// if we get a message from the timer, no matter what the answer is, it is not valid.
+		case <-timer.C: // Waiting for a message from this channel. Our code will block until it gets a message from this channel.
+			fmt.Printf("Time exceeded! You scored %d out of %d \n", correct, len(problems))
+			return
+		case answer := <-answerCh:
+			if answer == problem.answer {
+				correct += 1
+			}
 		}
+
 	}
 
 	fmt.Printf("You scored %d out of %d \n", correct, len(problems))
